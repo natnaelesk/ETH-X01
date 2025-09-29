a2001f1276afa53039f4b3aa3c5d0cd80a4a7425
natnael_esk <natnaeleskinder41@gmail.com>
2025-09-29 12:47:31 +0300

    try 76

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./" // very important for vercel static asset paths
+  base: "/", // ✅ correct for Vercel
+  build: {
+    outDir: "dist"
+  }
 });
